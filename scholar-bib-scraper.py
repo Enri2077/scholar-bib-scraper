e36a8c768bfb2409b3ac1ace3d42483603dde54d
Enrico Piazza <enry2077@gmail.com>
2019-10-03 14:54:55 +0100

    decreased wait time and added wait for login

--- scholar-bib-scraper.py
+++ scholar-bib-scraper.py
@@ -4,50 +4,48 @@
 import time 
 from selenium import webdriver
 from selenium.webdriver.support import ui
+import codecs
 
 
 def get_entries_on_file(driver, log_file='links.txt', bib_file='refs.bib'):
-    with open(log_file, 'a') as f: # for logging the current page
+    with codecs.open(log_file, 'a', 'utf-8') as f: # for logging the current page
         f.write(driver.current_url + '\n')
     # all these sleeps may not be really necessary
-    time.sleep(1)
+    time.sleep(0.1)
     select_all = wait.until(lambda driver: driver.find_element_by_id('gs_res_ab_xall'))
-    time.sleep(1)
+    time.sleep(0.1)
     select_all.click()
-    time.sleep(1)
+    time.sleep(0.1)
     export = wait.until(lambda driver: driver.find_element_by_id('gs_res_ab_exp-b'))
-    time.sleep(1)
+    time.sleep(0.1)
     export.click()
-    time.sleep(1)
+    time.sleep(0.1)
     bibtex = wait.until(lambda driver: driver.find_element_by_partial_link_text('BibTeX'))
-    time.sleep(1)
+    time.sleep(0.1)
     bibtex.click()
-    time.sleep(1)
+    time.sleep(0.1)
     body = wait.until(lambda driver: driver.find_element_by_tag_name('body'))
-    time.sleep(1)
+    time.sleep(0.1)
     text = body.text 
-    with open(bib_file, 'a') as f: # keep appending the bibtex entries to file
+    with codecs.open(bib_file, 'a', 'utf-8') as f: # keep appending the bibtex entries to file
         f.write(text + '\n\n')
 
 
 if __name__ == '__main__':
     # initialize the browser -- requires chromedriver   
-    driver = webdriver.Chrome()
+    driver = webdriver.Chrome(executable_path='/home/enrico/w/scholar-bib-scraper/chromedriver')
     # this waiting may not be necessary
     wait = ui.WebDriverWait(driver, 90) 
     driver.get('http://www.scholar.google.com/');
-
-    # complete login within 45 seconds
-    sign_in = driver.find_element_by_partial_link_text('SIGN');
-    sign_in.click()
-    time.sleep(45) 
+    
+    raw_input("Select the English language (EN-EN) for the page, log in and press enter in this console...")
 
     library = wait.until(lambda driver: driver.find_element_by_partial_link_text('library'))
     time.sleep(1)
     library.click()
     time.sleep(1)
     get_entries_on_file(driver) # get the 10 entries at the first page
-    time.sleep(90) # this is where I had to solve reCAPTCHA
+    time.sleep(1) # this is where I had to solve reCAPTCHA
 
     for i in range(1, 500): 
         # this loop breaks with some error after all done
